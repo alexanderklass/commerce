ea5319574cb1a4c3c3747c51334372bcf6fe91b7
alexanderklass <alexklass16@gmail.com>
2025-04-29 23:10:26 +0200

    change

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,23 @@
 export default {
+  basePath: "/commerce",
+  output: "export",
+  /*
   experimental: {
     ppr: false,
     inlineCss: true,
-    useCache: true
+    useCache: true,
   },
+
+   */
   images: {
-    formats: ['image/avif', 'image/webp'],
+    formats: ["image/avif", "image/webp"],
+    unoptimized: true,
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'cdn.shopify.com',
-        pathname: '/s/files/**'
-      }
-    ]
-  }
+        protocol: "https",
+        hostname: "cdn.shopify.com",
+        pathname: "/s/files/**",
+      },
+    ],
+  },
 };
