004570bee4802f09775a0763fdf0db6237767dba
alexanderklass <alexklass16@gmail.com>
2025-04-30 09:03:50 +0200

    change

--- next.config.js
+++ next.config.js
@@ -13,18 +13,6 @@ const nextConfig = {
     inlineCss: true,
     useCache: true,
   },
-
-  images: {
-    formats: ["image/avif", "image/webp"],
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn.shopify.com",
-        pathname: "/s/files/**",
-      },
-    ],
-  },
 };
 
 module.exports = nextConfig;
