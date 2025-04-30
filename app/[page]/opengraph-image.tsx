dede3d473e61b41cc7c16381be4dcf8115ce2dcd
alexanderklass <alexklass16@gmail.com>
2025-04-30 08:50:14 +0200

    change

--- app/[page]/opengraph-image.tsx
+++ app/[page]/opengraph-image.tsx
@@ -1,6 +1,9 @@
 import OpengraphImage from 'components/opengraph-image';
 import { getPage } from 'lib/shopify';
 
+export const dynamic = 'force-static';
+export const revalidate = false;
+
 export default async function Image({ params }: { params: { page: string } }) {
   const page = await getPage(params.page);
   const title = page.seo?.title || page.title;
